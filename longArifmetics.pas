2c3afd2b7eeafb2265aca634ad647eecec4ae771
linaverde <p.koroleva1102@gmail,com>
2018-10-17 11:55:26 +1000

    debug + new: print, invert

--- longArifmetics.pas
+++ longArifmetics.pas
@@ -5,11 +5,11 @@ const
   min = 3;
 
 type
-  dec = array [1..max + 1] of string[3];
+  dec = array [0..max + 1] of word;
   
   numbers = set of '0'..'7';
   
-  longDecimal =  dec;
+  longDecimal = dec;
   
   longReal = record
     nat: dec;
@@ -17,7 +17,6 @@ type
   end;
 
 var
-  f: Text;
   num := ['0'..'7'];
 
 function checkEight(s: string): string;
@@ -29,11 +28,13 @@ begin
       checkEight := 'Встречен недопустимый для восьмеричной системы символ';
 end;
 
+
 procedure readLongDec(var input: text; var a: longDecimal; var err: string);
 var
   i: integer;
   s: string;
 begin
+  err := '';
   readln(input, s);
   if s.Length < min then 
     err := 'Введенное число содержит на ' + (min - s.Length) + ' меньше знаков, чем допустимо';
@@ -41,42 +42,82 @@ begin
     err := 'Введенное число содержит на ' + (s.Length - max) + ' больше знаков, чем допустимо';
   if err = '' then err := checkEight(s);
   if err = '' then
+  begin
+    i := 0;
+    while s[1] = '0' do
+      delete(s, 1, 1);
+    if (s.Length mod 3) <> 0 then
+      if (s.Length mod 3) = 2 then
+      begin
+        i := i + 1;
+        a[i] := StrToInt(Copy(s, 1, 2));
+        a[0] := i;
+        Delete(s, 1, 2);
+      end
+      else
+      begin
+        i := i + 1;
+        a[i] := StrToInt(Copy(s, 1, 1));
+        a[0] := i;
+        Delete(s, 1, 1);
+      end;
     while s.Length > 0 do
     begin
       i := i + 1;
-      a[i + 1] := Copy(s, 1, 3);
-      a[1] := IntToStr(i);
+      a[i] := StrToInt(Copy(s, 1, 3));
+      a[0] := i;
       Delete(s, 1, 3);
     end
+  end;
 end;
 
 
 procedure readLongReal(var input: text; var b: longReal; var err: string);
-
-procedure convertString(var s: string; var a: dec);
-
+  
+  procedure convertString(var s: string; var a: dec; t: byte);
+  
   var
     i: integer;
-begin
-  if s.Length < min then 
-    err := 'Введенное число содержит на ' + (min - s.Length) + ' меньше знаков, чем допустимо';
-  if s.Length > max then 
-    err := 'Введенное число содержит на ' + (s.Length - max) + ' больше знаков, чем допустимо';
-  if err = '' then err := checkEight(s);
-  if err = '' then
-    while s.Length > 0 do
+  begin
+    if s.Length < min then 
+      err := 'Введенное число содержит на ' + (min - s.Length) + ' меньше знаков, чем допустимо';
+    if s.Length > max then 
+      err := 'Введенное число содержит на ' + (s.Length - max) + ' больше знаков, чем допустимо';
+    if err = '' then err := checkEight(s);
+    if err = '' then
     begin
-      i := i + 1;
-      a[i + 1] := Copy(s, 1, 3);
-      a[1] := IntToStr(i);
-      Delete(s, 1, 3);
-    end
-end;
+      i := 0;
+      if t = 1 then
+        if (s.Length mod 3) <> 0 then
+          if (s.Length mod 3) = 2 then
+          begin
+            i := i + 1;
+            a[i] := StrToInt(Copy(s, 1, 2));
+            a[0] := i;
+            Delete(s, 1, 2);
+          end
+          else
+          begin
+            i := i + 1;
+            a[i] := StrToInt(Copy(s, 1, 1));
+            a[0] := i;
+            Delete(s, 1, 1);
+          end;
+      while s.Length > 0 do
+      begin
+        i := i + 1;
+        a[i] := StrToInt(Copy(s, 1, 3));
+        a[0] := i;
+        Delete(s, 1, 3);
+      end
+    end;
+  end;
 
 var
   i, dot: integer;
   s, sNat: string;
 begin
+  err := '';
   readln(input, s);
   dot := 0;
   for i := 1 to s.Length do
@@ -90,22 +131,63 @@ begin
   begin
     sNat := Copy(s, 1, dot - 1);
     Delete(s, 1, dot);
-    convertString(sNat, b.nat);
+    while sNat[1] = '0' do
+      delete(sNat, 1, 1);
+    convertString(sNat, b.nat, 1);
     if err <> '' then
       err := 'Ошибка в целой части числа: ' + err;
   end;
   if err = '' then
   begin
-    convertString(s, b.frac);
+    while s[Length(s)] = '0' do
+      Delete(s, Length(s), 1);
+    convertString(s, b.frac, 2);
     if err <> '' then
       err := 'Ошибка в дробной части числа: ' + err
   end;  
-  
 end;
 
+procedure printLong(const a: dec);
 var
-  a: longDecimal;
-  b: longReal;
+  i: integer;
+begin
+  for i := 1 to a[0] do
+    write(a[i]);
+  writeln();
+end;
+
+procedure printLongReal(const a: longReal);
+var
+  i: integer;
+begin
+  for i := 1 to a.nat[0] do
+    write(a.nat[i]);
+  write(',');
+  for i := 1 to a.frac[0] do
+    write(a.frac[i]);
+  writeln();
+end;
+
+procedure invertArr(var arr: dec);
+var
+  i, k: integer;
+  temp: word;
+begin
+  k := arr[0] div 2;
+  for i := 1 to k do
+  begin
+    temp := arr[i];
+    arr[i] := arr[arr[0] + 1 - i];
+    arr[arr[0] + 1 - i] := temp;
+  end;
+end;
+
+
+
+
+var
+  a, c: longDecimal;
+  b, d: longReal;
   err: string;
   input, output: text;
 
@@ -114,9 +196,11 @@ begin
   reset(input);
   readLongDec(input, a, err);
   if err <> '' then 
-    Writeln (err);
+    Writeln(err);
   readLongReal(input, b, err);
   if err <> '' then 
-    Writeln (err);
+    Writeln(err);
   Close(input);
+  printLong(a);
+  printLongReal(b);
 end.
